194712548027d7c060a9fa8cf0d834a16d410210
en301 <emilsjostrand1000@gmail.com>
2023-11-22 09:59:17 +0100

    Add project files.

---
+++ uppgift 5.2 E/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3] { 1, 2, 0 };
+            Console.WriteLine("Skriv in ett tal");
+            tal[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine(tal[0] +" " + tal[1] +" " + tal[2]);
+            Console.ReadKey();
+        }
+    }
+}
